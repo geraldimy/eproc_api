26548014f11d07f0740feae9f8159b545ad20ba8
Geraldi Muhammad Yushar <geraldimy21@gmail.com>
2020-03-09 21:54:26 +0700

    added passport login & register

--- config/app.php
+++ config/app.php
@@ -161,7 +161,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Laravel\Passport\PassportServiceProvider::class,
+       
         /*
          * Package Service Providers...
          */
@@ -174,6 +175,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+       
+      
 
     ],
 
