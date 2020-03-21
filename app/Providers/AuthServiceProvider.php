7c03a4d235f49cfca4ce1ef56a8f4524baa826fa
Hardabell <hardabell@gmail.com>
2020-03-21 11:04:01 +0700

    expired token

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,5 +27,6 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+        Passport::personalAccessTokensExpireIn(now()->addMonths(2));
     }
 }
