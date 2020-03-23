b77692da2b428c0098513231cbbd67877dd3a3f8
Hardabell <hardabell@gmail.com>
2020-03-23 10:26:29 +0700

    migration database promo,RPK,product,category

---
+++ database/migrations/2020_03_23_031249_create_product_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->BigInteger('category');
+            $table->string('product_name');
+            $table->text('short_desc');
+            $table->text('long_desc');
+            $table->string('image');
+            $table->string('color');
+            $table->integer('price');
+            $table->string('unit');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
